e345ccc06059971c366d755354af48dd8baca1a0
Hyeonhui <khh0965@naver.com>
2025-07-15 18:04:52 +0900

    Create p2292.java

---
+++ Bronze/p2292.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class p2292 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new  OutputStreamWriter(System.out));
+
+
+    }
+}
