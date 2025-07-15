41b35c34e015fc920ff997727b269d1b5211be2f
Hyeonhui <khh0965@naver.com>
2025-07-15 18:29:01 +0900

    Update p2292.java

--- Bronze/p2292.java
+++ Bronze/p2292.java
@@ -3,8 +3,19 @@ import java.io.*;
 public class p2292 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new  OutputStreamWriter(System.out));
-
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=Integer.parseInt(br.readLine());
+        int route=0;
+        if (n!=1) {
+            for (int i = 1; i < n; i += 6*route) {
+                route++;
+            }
+        }
+        bw.write(String.valueOf(route)+1);
+        bw.flush();
+        bw.close();
     }
 }
+// 1  2 7  8 19  20 37 ...
+// 1   6    12     18 ...
+// 1+6*route
