d25ae1826353469f6337bce16217446f7072e468
Hyeonhui <khh0965@naver.com>
2025-08-08 01:13:55 +0900

    Update p2775.cpp

--- Bronze1/p2775.cpp
+++ Bronze1/p2775.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
- 
+
+void fill_apment(int arr[15][15]);
+
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     int t,k,n;
     cin >> t;
     int apment[15][15]={0,};
-    for(int i=1;i<15;i++){
-        apment[0][i]=i;
-    }
-    for(int i=1;i<15;i++){
-        for(int j=1;j<15;j++){
-            apment[i][j]=apment[i][j-1]+apment[i-1][j];
-        }
-    }
+
+    // 거주민 채우기
+    fill_apment(apment);
+
     for (int i=0;i<t;i++){
         cin >> k >> n;
         cout << apment[k][n] << "\n";
     }
-    
 }
-// 0층  1  2  3  4  5  6  7  ...
+
+void fill_apment(int arr[15][15]){
+    for(int i=1;i<15;i++){
+        arr[0][i]=i;
+    }
+    for(int i=1;i<15;i++){
+        for(int j=1;j<15;j++){
+            arr[i][j]=arr[i][j-1]+arr[i-1][j];
+        }
+    }
+}
+
+// 0층  1  2  3 ...
 // 1층  1  3  6 (k층 n-1호 + k-1층 n호)
 // 2층  1  4  10
